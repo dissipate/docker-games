7dfdcd4b5bee4dc3ce42ac9ed70607ebd15c2dbb
Steven Kane <dissipate@gmail.com>
2015-12-05 13:36:52 -0500

    set up communication with 2 mpbs

--- servers/matching-pennies/src/main.rs
+++ servers/matching-pennies/src/main.rs
@@ -5,21 +5,33 @@ fn main() {
 
 
     let mut context = zmq::Context::new();
-    let mut requester = context.socket(zmq::REQ).unwrap();
+    let mut mpb1 = context.socket(zmq::REQ).unwrap();
+    let mut mpb2 = context.socket(zmq::REQ).unwrap();
 
-    requester.set_rcvhwm(1).unwrap();
+    mpb1.set_rcvhwm(1).unwrap();
+    mpb2.set_rcvhwm(1).unwrap();
 
-    assert!(requester.connect("tcp://mpb:5555").is_ok());
+    assert!(mpb1.connect("tcp://mpb1:5555").is_ok());
 
-    let mut msg_req = zmq::Message::new().unwrap();
+    assert!(mpb2.connect("tcp://mpb2:5555").is_ok());
+
+    let mut msg_req1 = zmq::Message::new().unwrap();
+    let mut msg_req2 = zmq::Message::new().unwrap();
 
     loop {
 
-        println!("Sending Hello");
-        requester.send(b"Hello", 0).unwrap();
+        println!("Sending Hello1");
+        mpb1.send(b"Hello", 0).unwrap();
 
-        requester.recv(&mut msg_req, 0).unwrap();
-        println!("Received {}", msg_req.as_str().unwrap());
+        mpb1.recv(&mut msg_req1, 0).unwrap();
+        println!("Received {}", msg_req1.as_str().unwrap());
+
+        println!("Sending Hello2");
+        mpb2.send(b"Hello", 0).unwrap();
+
+        mpb2.recv(&mut msg_req2, 0).unwrap();
+        println!("Received {}", msg_req2.as_str().unwrap());
+
 
         thread::sleep_ms(1000);
     }

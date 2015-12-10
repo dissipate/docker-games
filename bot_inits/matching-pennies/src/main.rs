0a71d01bf359a4c9ba3f050ea4b4fb7ae5b7fd15
Steven Kane <dissipate@gmail.com>
2015-12-10 02:07:52 -0500

    got message pack working over zeromq, round trip

--- bot_inits/matching-pennies/src/main.rs
+++ bot_inits/matching-pennies/src/main.rs
@@ -1,7 +1,19 @@
 extern crate zmq;
 use std::thread;
 
+extern crate rmp_serialize as msgpack;
+extern crate rustc_serialize;
+
+use rustc_serialize::{Encodable, Decodable};
+use msgpack::{Encoder, Decoder};
+
 fn main() {
+
+    #[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
+    struct Pick{
+        p: String
+    }
+
     let mut ctx = zmq::Context::new();
 
     let mut responder = ctx.socket(zmq::REP).unwrap();
@@ -18,8 +30,13 @@ fn main() {
 
         let byte_msg = responder.recv_bytes(0).unwrap();
 
-        println!("Received {}", byte_msg.get(0).unwrap());
+        println!("Received {:?}", byte_msg);
+
+        let mut decoder = Decoder::new(&byte_msg[..]);
+        let res: Pick = Decodable::decode(&mut decoder).ok().unwrap();
         
+        println!("Received msgpack {:?}", res);
+
         responder.send_str("World", 0).unwrap();
         
         thread::sleep_ms(1000);
